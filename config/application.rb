af283c619a839f87889baa5b95fcd725842cfaa9
*CWLFZ6izhS <yachiyo.bsk.0221@icloud.com>
2022-04-18 05:15:22 +0900

    application.rb add

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Techpitgram
 
     config.i18n.default_locale = :ja
 
+    config.assets.initialize_on_precompile = true
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
