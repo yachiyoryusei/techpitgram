cc4ef4339ef27dde76737e72dc84103579f35e4a
*CWLFZ6izhS <yachiyo.bsk.0221@icloud.com>
2022-04-25 09:22:37 +0900

    user_name option add

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable
 
   validates :name,presence: true, length: {maximum: 50}
+  validates :user_name, uniqueness: true
 
   def update_without_current_password(params, *options)
     if params[:password].blank? && params[:password_confirmation].blank?
